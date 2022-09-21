255461424c5a77b04b7708bbedc13c9ef258a0a1
Sergiy Lytvyn <sergey.litvin.1994@gmail.com>
2022-09-21 12:37:54 +0300

    Task 2.5

--- index.js
+++ index.js
@@ -3,3 +3,28 @@
 //Task 2.1
 
 alert('Я - JavaScript!');
+
+//Task 2.4
+
+let admin,
+    name;
+
+name = 'Іван';
+admin = name;
+alert(admin);
+
+let ourPlanet = 'Земля',
+    userName;
+
+//const BIRTHDAY = '18.04.1982';
+//const age = someCode(BIRTHDAY);
+
+//Task 2.5
+
+let name2 = "Ілля";
+
+alert( `привіт ${1}` ); // привіт 1
+
+alert( `привіт ${"name"}` ); // привіт name
+
+alert( `привіт ${name2}` ); // ? привіт Ілля
