ce14055aa0d4f043aec16559651520f22ef160df
Sergiy Lytvyn <sergey.litvin.1994@gmail.com>
2022-12-12 20:44:29 +0200

    First react app

---
+++ my-app-first/src/Navigation.js
@@ -0,0 +1,16 @@
+function Navigation() {
+    return (
+    <div style={{display: "inline-block", width: "20%", verticalAlign: "top"}}>
+        <h2>Navigation</h2>
+        <ul>
+            <li>First</li>
+            <li>Second</li>
+            <li>Third</li>
+            <li>Fourth</li>
+            <li>Fifth</li>
+        </ul>
+    </div>
+    )
+}
+
+export default Navigation;
